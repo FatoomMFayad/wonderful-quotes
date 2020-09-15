4980ab5b5a4e3d08e6927d3554939f193baeac11
FatoomMFayad <fatoomfayyad@gmail.com>
2020-09-15 12:47:37 +0300

    add progressbar width

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,19 @@
 <template>
-    <div>
-        <h3>Quotes Added</h3>
-        <div class="progress">
-            <div class="progress-bar" role="progressbar" aria-valuenow="68" aria-valuemin="0" aria-valuemax="100">
-
+    <div class="row">
+        <div class="col-sm-12">
+            <h3>Quotes Added</h3>
+            <div class="progress">
+                <div class="progress-bar" role="progressbar" aria-valuenow="68" aria-valuemin="0" aria-valuemax="100"
+                :style="{ width: (quoteCount/maxQuote) * 100 + '%' }"
+                >
+                    {{ quoteCount }} / {{ maxQuote}}
+                </div>
             </div>
-        </div>
-    </div>
-    
+        </div>        
+    </div>    
 </template>
+<script>
+export default {
+   props: ['quoteCount', 'maxQuotes'] 
+}
+</script>
