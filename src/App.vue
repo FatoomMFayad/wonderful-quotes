50213fe1cc437d2a494ceaffa9b9c749b764192d
ffayad@ptcdb.edu.ps <ffayad@ptcdb.edu.ps>
2020-09-15 09:41:11 +0300

    passing data with props and slots

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
     <div class="container">
-        
+       <appQuoteGrid :quotes="Quotes"></appQuoteGrid> 
     </div>
 </template>
 
 <script>
+    import QuoteGrid from './components/QuoteGrid';
     export default {
         data: function() {
             return {
-                Quotes: ['Simple Quote'],
+                Quotes: [
+                    'Simple Quote'
+                ],
                 maxQuotes: 10
             }
+        },
+        components: {
+            appQuoteGrid: QuoteGrid
         }
         
     }
--- src/components/QuoteGrid.vue
+++ src/components/QuoteGrid.vue
@@ -1,11 +1,15 @@
 <template>
     <div class="row">
-
+        <appQuote v-for="quote in quotes">{{ quote }}</appQuote>
     </div>
 </template>
 <script>
+import Quote from  './Quote.vue';
 export default {
-    props:['quotes']    
+    props:['quotes'],
+    components: {
+        appQuote: Quote
+    }   
 }
 </script>
 <style>
