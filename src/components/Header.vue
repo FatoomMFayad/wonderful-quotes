d35d725d8bdcf334160ca2f75384d9267828956e
FatoomMFayad <fatoomfayyad@gmail.com>
2020-09-15 12:36:59 +0300

    add progressbar to header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,11 @@
 <template>
+    <div>
+        <h3>Quotes Added</h3>
+        <div class="progress">
+            <div class="progress-bar" role="progressbar" aria-valuenow="68" aria-valuemin="0" aria-valuemax="100">
+
+            </div>
+        </div>
+    </div>
     
 </template>
