718b0fbad855bc8473bbe0a4486ee9281f5c5924
ffayad@ptcdb.edu.ps <ffayad@ptcdb.edu.ps>
2020-09-15 09:32:52 +0300

    adding application components

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
     <div class="container">
-    
+        
     </div>
 </template>
 
 <script>
     export default {
+        data: function() {
+            return {
+                Quotes: ['Simple Quote'],
+                maxQuotes: 10
+            }
+        }
         
     }
 </script>
---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="row">
+
+    </div>
+</template>
+<script>
+export default {
+    props:['quotes']    
+}
+</script>
+<style>
+   
+</style>
